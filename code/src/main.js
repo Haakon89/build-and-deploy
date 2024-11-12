327ad9c86ad8de09ad59cfe1aa73ac8701c81a6d
Haakon89 <117762832+Haakon89@users.noreply.github.com>
2024-11-12 18:49:13 +0100

    added minutes left in year

--- code/src/main.js
+++ code/src/main.js
@@ -1,12 +1,14 @@
-import {  getSecondsLeftOfYear, getTimeString } from "./clock";
+import {  getSecondsLeftOfYear, getTimeString, getMinutesLeftOfYear } from "./clock";
 
 const timeContainer = document.getElementById("time");
 const timeLeft = document.getElementById("seconds-left");
+const timeLeftMin = document.getElementById("minutes-left");
 
 const render = () => {
   const now = new Date();
   timeLeft.innerText = getSecondsLeftOfYear(now);
   timeContainer.innerText = getTimeString(now);
+  timeLeftMin.innerText = getMinutesLeftOfYear(now);
 }
 
 render();
